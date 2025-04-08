1ad97af76ec3ab9b8f1c6860b6c11458a96126a9
Hai Chu <haichu@HaiChuPC>
2025-04-08 10:50:05 +0300

    update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import cv2
-import numpy as np
+# import numpy as np
 import av
 import asyncio
 from aiortc import RTCPeerConnection, RTCSessionDescription, MediaStreamTrack
@@ -64,53 +64,53 @@ class ProcessedVideoTrack(MediaStreamTrack):
         is_right_elbow_in = False
 
         processed_img = img
-        # if self.frame_count % self.frame_rate == 0:
-        #     self.frame_count = 0
-        #     feed = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        if self.frame_count % self.frame_rate == 0:
+            self.frame_count = 0
+            feed = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-        #     results = model(feed, verbose=False)
-        #     keypoints = results[0].keypoints.xy.cpu().numpy() if results[0].keypoints else []
+            results = model(feed, verbose=False)
+            keypoints = results[0].keypoints.xy.cpu().numpy() if results[0].keypoints else []
 
-        # for kp in keypoints:
-        #     if len(kp):
-        #         is_left_ear_in = not (kp[3][0] == 0 and kp[3][1] == 0)
-        #         is_right_ear_in = not (kp[4][0] == 0 and kp[4][1] == 0)
-        #         is_left_shoulder_in = not (kp[5][0] == 0 and kp[5][1] == 0)
-        #         is_right_shoulder_in = not (kp[6][0] == 0 and kp[6][1] == 0)
-        #         is_left_elbow_in = not (kp[7][0] == 0 and kp[7][1] == 0)
-        #         is_right_elbow_in = not (kp[8][0] == 0 and kp[8][1] == 0)
-        #     for idx in range(len(kp)):            
-        #         if idx is not None or idx in [5,6,7,8,9,10]:
-        #             x,y = kp[idx]
-        #             processed_img = cv2.circle(processed_img, (int(x), int(y)), 5, (0, 255, 0), -1)
-
-
-        # if is_left_ear_in and is_left_shoulder_in:
-        #     color = (0,0,255)
-        #     x1, y1 = kp[3]
-        #     x2, y2 = kp[5]
-        #     upleft = (int(x2), int(y1-100))
-        #     downright = (int(x2+100), int(y1))
-        #     if is_left_elbow_in:
-        #         x_elbow, y_elbow = kp[7]
-        #         if upleft[0] < x_elbow < downright[0] and upleft[1] < y_elbow < downright[1]: # elbow inside square
-        #             color = (0,255,0)
-        #     processed_img = cv2.rectangle(processed_img, upleft, downright, color, 2)
+        for kp in keypoints:
+            if len(kp):
+                is_left_ear_in = not (kp[3][0] == 0 and kp[3][1] == 0)
+                is_right_ear_in = not (kp[4][0] == 0 and kp[4][1] == 0)
+                is_left_shoulder_in = not (kp[5][0] == 0 and kp[5][1] == 0)
+                is_right_shoulder_in = not (kp[6][0] == 0 and kp[6][1] == 0)
+                is_left_elbow_in = not (kp[7][0] == 0 and kp[7][1] == 0)
+                is_right_elbow_in = not (kp[8][0] == 0 and kp[8][1] == 0)
+            for idx in range(len(kp)):            
+                if idx is not None or idx in [5,6,7,8,9,10]:
+                    x,y = kp[idx]
+                    processed_img = cv2.circle(processed_img, (int(x), int(y)), 5, (0, 255, 0), -1)
 
 
-        # if is_right_ear_in and is_right_shoulder_in:
-        #     color = (0,0,255)
-        #     x1, y1 = kp[4]
-        #     x2, y2 = kp[6]
-        #     upleft = (int(x2-100), int(y1-100))
-        #     downright = (int(x2), int(y1))
-        #     if is_right_elbow_in:
-        #         x_elbow, y_elbow = kp[8]
-        #         if upleft[0] < x_elbow < downright[0] and upleft[1] < y_elbow < downright[1]: # elbow inside square
-        #             color = (0,255,0)
-        #     processed_img = cv2.rectangle(processed_img, upleft, downright, color, 2)
+        if is_left_ear_in and is_left_shoulder_in:
+            color = (0,0,255)
+            x1, y1 = kp[3]
+            x2, y2 = kp[5]
+            upleft = (int(x2), int(y1-100))
+            downright = (int(x2+100), int(y1))
+            if is_left_elbow_in:
+                x_elbow, y_elbow = kp[7]
+                if upleft[0] < x_elbow < downright[0] and upleft[1] < y_elbow < downright[1]: # elbow inside square
+                    color = (0,255,0)
+            processed_img = cv2.rectangle(processed_img, upleft, downright, color, 2)
 
-        # processed_img = cv2.flip(processed_img, 1)
+
+        if is_right_ear_in and is_right_shoulder_in:
+            color = (0,0,255)
+            x1, y1 = kp[4]
+            x2, y2 = kp[6]
+            upleft = (int(x2-100), int(y1-100))
+            downright = (int(x2), int(y1))
+            if is_right_elbow_in:
+                x_elbow, y_elbow = kp[8]
+                if upleft[0] < x_elbow < downright[0] and upleft[1] < y_elbow < downright[1]: # elbow inside square
+                    color = (0,255,0)
+            processed_img = cv2.rectangle(processed_img, upleft, downright, color, 2)
+
+        processed_img = cv2.flip(processed_img, 1)
 
         # Encode frame back
         new_frame = av.VideoFrame.from_ndarray(processed_img, format="bgr24")
