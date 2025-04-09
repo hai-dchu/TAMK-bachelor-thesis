8ea2ae229933196d58d1caa6572a9c898482160d
Hai Chu <haichu@HaiChuPC>
2025-04-09 18:50:20 +0300

    test simple backend

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import cv2
-# import numpy as np
-import av
-import asyncio
-from aiortc import RTCPeerConnection, RTCSessionDescription, MediaStreamTrack, RTCConfiguration, RTCIceServer
-from ultralytics import YOLO
-from contextlib import asynccontextmanager
-from dotenv import load_dotenv
-load_dotenv()
+
+from flask import Flask, request, jsonify
+from flask_sockets import Sockets
+
+# app = Flask(__name__)
+# sockets = Sockets(app)
 
 app = FastAPI()
 app.add_middleware(
@@ -19,173 +16,24 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
-model = YOLO("yolo11n-pose.pt")
-test_img = cv2.imread('./test.jpg') # np.zeros((640, 480, 3), dtype=np.uint8)  # Empty black image
-test_result = model(test_img)
-if test_result:
-    print("YOLO model loaded successfully!")
-else:
-    print("Warning: YOLO model failed to load!")
+from aiortc import RTCPeerConnection, MediaStreamTrack
 
-pcs = set()
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    """Handles startup and shutdown cleanup"""
-    yield  # This runs while the app is running
-    print("Shutting down: Closing all WebRTC connections...")
-    coros = [pc.close() for pc in pcs]
-    await asyncio.gather(*coros)
-    pcs.clear()
-    print("Shutdown complete.")
+@app.route('/api/pose', methods=['POST'])
+def handle_offer():
+    offer = request.json['sdp']
+    pc = RTCPeerConnection()
+    pc.setRemoteDescription(offer)
+    # Create an answer
+    answer = pc.createAnswer()
+    pc.setLocalDescription(answer)
+    return jsonify({'sdp': pc.localDescription.sdp})
 
 
-class ProcessedVideoTrack(MediaStreamTrack):
-    kind = "video"
+# @sockets.route('/websocket')
+# def echo_socket(ws):
+#     while not ws.closed:
+#         message = ws.receive()
+#         ws.send(message)  # Echo the websocket message back to the client
 
-    def __init__(self, track, frame_rate=15):
-        super().__init__()
-        self.track = track
-        self.frame_count = 0
-        self.frame_rate = frame_rate
-        self.keypoints = []
-
-    async def recv(self):
-        frame = await self.track.recv()
-        # img = frame.to_ndarray(format="bgr24")
-
-        # self.frame_count += 1
-
-        # is_left_ear_in = False
-        # is_right_ear_in = False
-        # is_left_shoulder_in = False
-        # is_right_shoulder_in = False
-        # is_left_elbow_in = False
-        # is_right_elbow_in = False
-
-        # processed_img = img
-        # if self.frame_count % self.frame_rate == 0:
-        #     self.frame_count = 0
-        #     feed = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-        #     results = model(feed, verbose=False)
-        #     keypoints = results[0].keypoints.xy.cpu().numpy() if results[0].keypoints else []
-
-        # for kp in keypoints:
-        #     if len(kp):
-        #         is_left_ear_in = not (kp[3][0] == 0 and kp[3][1] == 0)
-        #         is_right_ear_in = not (kp[4][0] == 0 and kp[4][1] == 0)
-        #         is_left_shoulder_in = not (kp[5][0] == 0 and kp[5][1] == 0)
-        #         is_right_shoulder_in = not (kp[6][0] == 0 and kp[6][1] == 0)
-        #         is_left_elbow_in = not (kp[7][0] == 0 and kp[7][1] == 0)
-        #         is_right_elbow_in = not (kp[8][0] == 0 and kp[8][1] == 0)
-        #     for idx in range(len(kp)):            
-        #         if idx is not None or idx in [5,6,7,8,9,10]:
-        #             x,y = kp[idx]
-        #             processed_img = cv2.circle(processed_img, (int(x), int(y)), 5, (0, 255, 0), -1)
-
-
-        # if is_left_ear_in and is_left_shoulder_in:
-        #     color = (0,0,255)
-        #     x1, y1 = kp[3]
-        #     x2, y2 = kp[5]
-        #     upleft = (int(x2), int(y1-100))
-        #     downright = (int(x2+100), int(y1))
-        #     if is_left_elbow_in:
-        #         x_elbow, y_elbow = kp[7]
-        #         if upleft[0] < x_elbow < downright[0] and upleft[1] < y_elbow < downright[1]: # elbow inside square
-        #             color = (0,255,0)
-        #     processed_img = cv2.rectangle(processed_img, upleft, downright, color, 2)
-
-
-        # if is_right_ear_in and is_right_shoulder_in:
-        #     color = (0,0,255)
-        #     x1, y1 = kp[4]
-        #     x2, y2 = kp[6]
-        #     upleft = (int(x2-100), int(y1-100))
-        #     downright = (int(x2), int(y1))
-        #     if is_right_elbow_in:
-        #         x_elbow, y_elbow = kp[8]
-        #         if upleft[0] < x_elbow < downright[0] and upleft[1] < y_elbow < downright[1]: # elbow inside square
-        #             color = (0,255,0)
-        #     processed_img = cv2.rectangle(processed_img, upleft, downright, color, 2)
-
-        # processed_img = cv2.flip(processed_img, 1)
-
-        # # Encode frame back
-        # new_frame = av.VideoFrame.from_ndarray(processed_img, format="bgr24")
-
-        # # Sync timestamps (important for WebRTC)
-        # new_frame.pts = frame.pts
-        # new_frame.time_base = frame.time_base
-
-        return frame
-
-@app.get("/api")
-async def hello_world():
-    return {"message": "Hello World"}
-
-@app.post("/api/pose")
-async def offer(offer: dict):
-    try:
-        # iceServers = [
-        #     RTCIceServer(urls="stun:stun.relay.metered.ca:80"),
-        #     RTCIceServer(
-        #         urls="turn:global.relay.metered.ca:80",
-        #         username="3e1062646b1d379aae1703ee",
-        #         credential="VQtqDLm0SITNhgQ2"
-        #     ),
-        #     RTCIceServer(
-        #         urls="turn:global.relay.metered.ca:80",
-        #         username="3e1062646b1d379aae1703ee",
-        #         credential="VQtqDLm0SITNhgQ2",
-        #     ),
-        #     RTCIceServer(
-        #         urls="turn:global.relay.metered.ca:80?transport=tcp",
-        #         username="3e1062646b1d379aae1703ee",
-        #         credential="VQtqDLm0SITNhgQ2",
-        #     ),
-        #     RTCIceServer(
-        #         urls="turn:global.relay.metered.ca:443",
-        #         username="3e1062646b1d379aae1703ee",
-        #         credential="VQtqDLm0SITNhgQ2",
-        #     ),
-        #     RTCIceServer(
-        #         urls="turns:global.relay.metered.ca:443?transport=tcp",
-        #         username="3e1062646b1d379aae1703ee",
-        #         credential="VQtqDLm0SITNhgQ2",
-        #     ),
-        # ]
-        # config = RTCConfiguration(iceServers=iceServers)
-        pc = RTCPeerConnection()
-        pcs.add(pc)
-
-        @pc.on("track")
-        def on_track(track):
-            print(f"Received track: {track.kind}")
-            if track.kind == "video":
-                print("Processing video track")
-                processedTrack = ProcessedVideoTrack(track, 6)
-
-                pc.addTrack(processedTrack)
-
-        remote_description = RTCSessionDescription(sdp=offer["sdp"], type=offer["type"])
-        await pc.setRemoteDescription(remote_description)
-
-        answer = await pc.createAnswer()
-        await pc.setLocalDescription(answer)
-
-        while pc.iceGatheringState != "complete":
-            await asyncio.sleep(0.1)
-
-        return {"sdp": pc.localDescription.sdp, "type": pc.localDescription.type}
-    except Exception as e:
-        print(f"error in offer(): {e}")
-        return {"error": str(e)}
-
-
-from routes.users import router as users_router
-from routes.auth import router as auth_router
-
-app.include_router(users_router)
-app.include_router(auth_router)
+if __name__ == "__main__":
+    app.run()
--- frontend/src/components/PoseEstimation.jsx
+++ frontend/src/components/PoseEstimation.jsx
@@ -1,93 +1,109 @@
 import React, { useEffect, useRef, useState } from "react";
 import SimplePeer from "simple-peer/simplepeer.min.js";
-import { Stream } from "readable-stream";
+// import { Stream } from "readable-stream";
 
 import "./PoseEstimation.css";
-import { config } from "process";
-// import Webcam from "react-webcam";
 
-window.ReadableStream = window.ReadableStream || Stream.Readable;
-window.DuplexStream = window.DuplexStream || Stream.Duplex;
+// window.ReadableStream = window.ReadableStream || Stream.Readable;
+// window.DuplexStream = window.DuplexStream || Stream.Duplex;
 
 const PoseEstimation = () => {
 	const webcamRef = useRef(null);
 	const videoRef = useRef(null);
 	const [peer, setPeer] = useState(null);
 
-	useEffect(() => {
-		const startWebRTC = async () => {
-			const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+	const pc = new RTCPeerConnection();
 
-			if (webcamRef.current) webcamRef.current.srcObject = stream;
+	pc.ontrack = event => {
+		if (event.streams && event.streams[0]) {
+			videoRef.current.srcObject = event.streams[0];
+		}
+	};
 
-			const p = new SimplePeer({
-				initiator: true,
-				trickle: false,
-				stream,
-				config: {
-					// iceServers: [
-					// 	{
-					// 	  urls: "stun:stun.relay.metered.ca:80",
-					// 	},
-					// 	{
-					// 	  urls: "turn:global.relay.metered.ca:80",
-					// 	  username: "3e1062646b1d379aae1703ee",
-					// 	  credential: "VQtqDLm0SITNhgQ2",
-					// 	},
-					// 	{
-					// 	  urls: "turn:global.relay.metered.ca:80?transport=tcp",
-					// 	  username: "3e1062646b1d379aae1703ee",
-					// 	  credential: "VQtqDLm0SITNhgQ2",
-					// 	},
-					// 	{
-					// 	  urls: "turn:global.relay.metered.ca:443",
-					// 	  username: "3e1062646b1d379aae1703ee",
-					// 	  credential: "VQtqDLm0SITNhgQ2",
-					// 	},
-					// 	{
-					// 	  urls: "turns:global.relay.metered.ca:443?transport=tcp",
-					// 	  username: "3e1062646b1d379aae1703ee",
-					// 	  credential: "VQtqDLm0SITNhgQ2",
-					// 	},
-					// ],
-				}
+	function startConnection() {
+		navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+			.then(stream => {
+				videoRef.current.srcObject = stream;
+				stream.getTracks().forEach(track => pc.addTrack(track, stream));
 			});
-			p.on("signal", async (data) => {
-				console.log("SIGNAL", data);
-				const response = await fetch(`${import.meta.env.VITE_BASE_URL}/pose`, {
-					method: "POST",
-					headers: { "Content-Type": "application/json" },
-					body: JSON.stringify(data),
-				});
-				const answer = await response.json();
-				console.log(answer);
+		// Signaling code to exchange offers/answers and candidates
+	}
 
-				p.signal(answer);
-			});
+	// useEffect(() => {
+	// 	const startWebRTC = async () => {
+	// 		const stream = await navigator.mediaDevices.getUserMedia({ video: true });
 
-			p.on("stream", (remoteStream) => {
-				if (videoRef.current) {
-					videoRef.current.srcObject = remoteStream;
-				}
-			});
+	// 		if (webcamRef.current) webcamRef.current.srcObject = stream;
 
-			p.on("error", (err) => {
-				console.log("Error", err);
-			});
 
-			p.on('iceStateChange', (state) => {
-				console.log('ICE state changed:', state);
-				}
-			);
+	// 		const p = new SimplePeer({
+	// 			initiator: true,
+	// 			trickle: false,
+	// 			stream: stream,
+	// 			// config: {
+	// 				// iceServers: [
+	// 				// 	{
+	// 				// 	  urls: "stun:stun.relay.metered.ca:80",
+	// 				// 	},
+	// 				// 	{
+	// 				// 	  urls: "turn:global.relay.metered.ca:80",
+	// 				// 	  username: "3e1062646b1d379aae1703ee",
+	// 				// 	  credential: "VQtqDLm0SITNhgQ2",
+	// 				// 	},
+	// 				// 	{
+	// 				// 	  urls: "turn:global.relay.metered.ca:80?transport=tcp",
+	// 				// 	  username: "3e1062646b1d379aae1703ee",
+	// 				// 	  credential: "VQtqDLm0SITNhgQ2",
+	// 				// 	},
+	// 				// 	{
+	// 				// 	  urls: "turn:global.relay.metered.ca:443",
+	// 				// 	  username: "3e1062646b1d379aae1703ee",
+	// 				// 	  credential: "VQtqDLm0SITNhgQ2",
+	// 				// 	},
+	// 				// 	{
+	// 				// 	  urls: "turns:global.relay.metered.ca:443?transport=tcp",
+	// 				// 	  username: "3e1062646b1d379aae1703ee",
+	// 				// 	  credential: "VQtqDLm0SITNhgQ2",
+	// 				// 	},
+	// 				// ],
+	// 			// }
+	// 		});
+	// 		p.on("signal", async (data) => {
+	// 			console.log("SIGNAL", data);
+	// 			const response = await fetch(`${import.meta.env.VITE_BASE_URL}/pose`, {
+	// 				method: "POST",
+	// 				headers: { "Content-Type": "application/json" },
+	// 				body: JSON.stringify(data),
+	// 			});
+	// 			const answer = await response.json();
+	// 			console.log(answer);
 
-			setPeer(p);
-		};
+	// 			p.signal(answer);
+	// 		});
 
-		startWebRTC();
-		return () => {
-			peer?.destroy();
-		};
-	}, []);
+	// 		p.on("stream", (remoteStream) => {
+	// 			if (videoRef.current) {
+	// 				videoRef.current.srcObject = remoteStream;
+	// 			}
+	// 		});
+
+	// 		p.on("error", (err) => {
+	// 			console.log("Error", err);
+	// 		});
+
+	// 		p.on('iceStateChange', (state) => {
+	// 			console.log('ICE state changed:', state);
+	// 			}
+	// 		);
+
+	// 		setPeer(p);
+	// 	};
+
+	// 	startWebRTC();
+	// 	return () => {
+	// 		peer?.destroy();
+	// 	};
+	// }, []);
 
 	return (
 		<div style={{
@@ -96,18 +112,19 @@ const PoseEstimation = () => {
 			alignItems: "center",
 			justifyContent: "center",
 		}}>
-			<video
+			{/* <video
 				ref={webcamRef}
 				autoPlay
 				playsInline
 				style={{ width: "640px", height: "480px" }}
-			/>
+				/> */}
 			<video
 				ref={videoRef}
 				autoPlay
 				playsInline
 				style={{ width: "640px", height: "480px" }}
 			/>
+			<button onClick={startConnection}>click here</button>
 		</div>
 	);
 };
