95d92fcc4e787053a644d4f7762108205547eb04
Hai Chu <haichu@HaiChuPC>
2025-04-10 14:30:41 +0300

    update backend

--- backend/app.py
+++ backend/app.py
@@ -33,74 +33,74 @@ class PoseRecTrack(MediaStreamTrack):
 
     async def recv(self):
         frame = await self.track.recv()
-        img = frame.to_ndarray(format="bgr24")
+        # img = frame.to_ndarray(format="bgr24")
 
-        self.frame_count += 1
+        # self.frame_count += 1
 
-        is_left_ear_in = False
-        is_right_ear_in = False
-        is_left_shoulder_in = False
-        is_right_shoulder_in = False
-        is_left_elbow_in = False
-        is_right_elbow_in = False
+        # is_left_ear_in = False
+        # is_right_ear_in = False
+        # is_left_shoulder_in = False
+        # is_right_shoulder_in = False
+        # is_left_elbow_in = False
+        # is_right_elbow_in = False
 
-        processed_img = img
-        if self.frame_count % self.frame_rate == 0:
-            self.frame_count = 0
-            feed = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        # processed_img = img
+        # if self.frame_count % self.frame_rate == 0:
+        #     self.frame_count = 0
+        #     feed = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-            results = model(feed, verbose=False)
-            self.keypoints = results[0].keypoints.xy.cpu().numpy() if results[0].keypoints else []
+        #     results = model(feed, verbose=False)
+        #     self.keypoints = results[0].keypoints.xy.cpu().numpy() if results[0].keypoints else []
 
-        for kp in self.keypoints:
-            if len(kp):
-                is_left_ear_in = not (kp[3][0] == 0 and kp[3][1] == 0)
-                is_right_ear_in = not (kp[4][0] == 0 and kp[4][1] == 0)
-                is_left_shoulder_in = not (kp[5][0] == 0 and kp[5][1] == 0)
-                is_right_shoulder_in = not (kp[6][0] == 0 and kp[6][1] == 0)
-                is_left_elbow_in = not (kp[7][0] == 0 and kp[7][1] == 0)
-                is_right_elbow_in = not (kp[8][0] == 0 and kp[8][1] == 0)
-            for idx in range(len(kp)):            
-                if idx is not None or idx in [5,6,7,8,9,10]:
-                    x,y = kp[idx]
-                    processed_img = cv2.circle(processed_img, (int(x), int(y)), 5, (0, 255, 0), -1)
-
-
-        if is_left_ear_in and is_left_shoulder_in:
-            color = (0,0,255)
-            x1, y1 = kp[3]
-            x2, y2 = kp[5]
-            upleft = (int(x2), int(y1-100))
-            downright = (int(x2+100), int(y1))
-            if is_left_elbow_in:
-                x_elbow, y_elbow = kp[7]
-                if upleft[0] < x_elbow < downright[0] and upleft[1] < y_elbow < downright[1]: # elbow inside square
-                    color = (0,255,0)
-            processed_img = cv2.rectangle(processed_img, upleft, downright, color, 2)
+        # for kp in self.keypoints:
+        #     if len(kp):
+        #         is_left_ear_in = not (kp[3][0] == 0 and kp[3][1] == 0)
+        #         is_right_ear_in = not (kp[4][0] == 0 and kp[4][1] == 0)
+        #         is_left_shoulder_in = not (kp[5][0] == 0 and kp[5][1] == 0)
+        #         is_right_shoulder_in = not (kp[6][0] == 0 and kp[6][1] == 0)
+        #         is_left_elbow_in = not (kp[7][0] == 0 and kp[7][1] == 0)
+        #         is_right_elbow_in = not (kp[8][0] == 0 and kp[8][1] == 0)
+        #     for idx in range(len(kp)):            
+        #         if idx is not None or idx in [5,6,7,8,9,10]:
+        #             x,y = kp[idx]
+        #             processed_img = cv2.circle(processed_img, (int(x), int(y)), 5, (0, 255, 0), -1)
 
 
-        if is_right_ear_in and is_right_shoulder_in:
-            color = (0,0,255)
-            x1, y1 = kp[4]
-            x2, y2 = kp[6]
-            upleft = (int(x2-100), int(y1-100))
-            downright = (int(x2), int(y1))
-            if is_right_elbow_in:
-                x_elbow, y_elbow = kp[8]
-                if upleft[0] < x_elbow < downright[0] and upleft[1] < y_elbow < downright[1]: # elbow inside square
-                    color = (0,255,0)
-            processed_img = cv2.rectangle(processed_img, upleft, downright, color, 2)
+        # if is_left_ear_in and is_left_shoulder_in:
+        #     color = (0,0,255)
+        #     x1, y1 = kp[3]
+        #     x2, y2 = kp[5]
+        #     upleft = (int(x2), int(y1-100))
+        #     downright = (int(x2+100), int(y1))
+        #     if is_left_elbow_in:
+        #         x_elbow, y_elbow = kp[7]
+        #         if upleft[0] < x_elbow < downright[0] and upleft[1] < y_elbow < downright[1]: # elbow inside square
+        #             color = (0,255,0)
+        #     processed_img = cv2.rectangle(processed_img, upleft, downright, color, 2)
 
-        processed_img = cv2.flip(processed_img, 1)
 
-        # Encode frame back
-        new_frame = av.VideoFrame.from_ndarray(processed_img, format="bgr24")
+        # if is_right_ear_in and is_right_shoulder_in:
+        #     color = (0,0,255)
+        #     x1, y1 = kp[4]
+        #     x2, y2 = kp[6]
+        #     upleft = (int(x2-100), int(y1-100))
+        #     downright = (int(x2), int(y1))
+        #     if is_right_elbow_in:
+        #         x_elbow, y_elbow = kp[8]
+        #         if upleft[0] < x_elbow < downright[0] and upleft[1] < y_elbow < downright[1]: # elbow inside square
+        #             color = (0,255,0)
+        #     processed_img = cv2.rectangle(processed_img, upleft, downright, color, 2)
 
-        # Sync timestamps (important for WebRTC)
-        new_frame.pts = frame.pts
-        new_frame.time_base = frame.time_base
+        # processed_img = cv2.flip(processed_img, 1)
 
-        return new_frame
+        # # Encode frame back
+        # new_frame = av.VideoFrame.from_ndarray(processed_img, format="bgr24")
+
+        # # Sync timestamps (important for WebRTC)
+        # new_frame.pts = frame.pts
+        # new_frame.time_base = frame.time_base
+
+        return frame
 
 app = FastAPI()
 app.add_middleware(
@@ -150,6 +150,7 @@ async def handle_offer(offer: dict):
         @pc.on('track')
         def on_track(track):
             if track.kind == 'video':
+                
                 processed_track = PoseRecTrack(track)
                 pc.addTrack(processed_track)
 
